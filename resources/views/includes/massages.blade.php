28edc4f19a4473ac2ba1e428a73b9b6ef3d1f5be
Adham Khalifa <52078922+AK1721@users.noreply.github.com>
2020-08-17 21:23:38 +0200

    Delete massages.blade.php
    non-useful file

--- resources/views/includes/massages.blade.php
+++
@@ -1,9 +0,0 @@
-@if (count($errors) > 0)
-  @foreach ($errors->all() as $error)
-    <div class="alert alert-dismissible alert-danger">{{$error}}</div>
-  @endforeach
-@endif
-
-@if (Session::has('massege'))
-  <div class="alert alert-dismissible alert-success">{{Session::get('massage')}}</div>
-@endif
